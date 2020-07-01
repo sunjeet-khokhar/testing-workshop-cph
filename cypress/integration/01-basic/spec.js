e094d42c200328270cb16d0b72723bd3c20aeb90
Sunjeet Khokhar <sunjeet81@gmail.com>
2020-07-01 13:13:46 +1200

    del
    delete

--- cypress/integration/01-basic/spec.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-// @ts-check
-it('loads', () => {
-  cy.visit('https://seequentglobal--protodev.my.salesforce.com/')
-  // use "selector", "text" arguments to "cy.contains"
-  cy.contains('salesforce')
-})
